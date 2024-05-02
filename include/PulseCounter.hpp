95c844ca86ab1479c050bbf8faf07b8cde69ae67
Chikurin1014 <chikurin1014@gmail.com>
2024-05-03 02:13:27 +0900

    modified pulse counter

--- include/PulseCounter.hpp
+++ include/PulseCounter.hpp
@@ -4,15 +4,13 @@
 
 #include "driver/pcnt.h"
 
-struct PulseCounter {
-    uint8_t pulseInputPin;
-    uint8_t pulseControlPin;
-    pcnt_channel_t pulseCounterChannel;
-    pcnt_unit_t pulseCounterUnit;
-    uint8_t pulseCountLowerLimit;
-    uint8_t pulseCountUpperLimit;
-    bool incrementOnPositiveEdge;
-
+template<uint8_t PULSE_INPUT_PIN,
+         uint8_t PULSE_CTRL_PIN,
+         pcnt_channel_t PULSE_COUNTER_CHANNEL,
+         pcnt_unit_t PULSE_COUNTER_UNIT,
+         bool INCREMENT_ON_POSITIVE_EDGE>
+class PulseCounter {
+public:
     void setup() {
         config();
         pause();
@@ -20,30 +18,33 @@ struct PulseCounter {
     }
     void config() {
         pcnt_config_t config;
-        config.pulse_gpio_num = pulseInputPin;
-        config.ctrl_gpio_num = pulseControlPin;
-        if (incrementOnPositiveEdge) {
+        config.pulse_gpio_num = PULSE_INPUT_PIN;
+        config.ctrl_gpio_num = PULSE_CTRL_PIN;
+        if (INCREMENT_ON_POSITIVE_EDGE) {
             config.lctrl_mode = PCNT_MODE_KEEP;
             config.hctrl_mode = PCNT_MODE_REVERSE;
         } else {
             config.lctrl_mode = PCNT_MODE_REVERSE;
             config.hctrl_mode = PCNT_MODE_KEEP;
         }
-        config.channel = pulseCounterChannel;
-        config.unit = pulseCounterUnit;
+        config.channel = PULSE_COUNTER_CHANNEL;
+        config.unit = PULSE_COUNTER_UNIT;
         config.pos_mode = PCNT_COUNT_INC;
         config.neg_mode = PCNT_COUNT_DEC;
-        config.counter_l_lim = pulseCountLowerLimit;
-        config.counter_h_lim = pulseCountUpperLimit;
         pcnt_unit_config(&config);
     }
     void pause() {
-        pcnt_counter_pause(pulseCounterUnit);
+        pcnt_counter_pause(PULSE_COUNTER_UNIT);
     }
     void clear() {
-        pcnt_counter_clear(pulseCounterUnit);
+        pcnt_counter_clear(PULSE_COUNTER_UNIT);
     }
     void resume() {
-        pcnt_counter_resume(pulseCounterUnit);
+        pcnt_counter_resume(PULSE_COUNTER_UNIT);
+    }
+    int16_t getCount() {
+        int16_t count;
+        pcnt_get_counter_value(PULSE_COUNTER_UNIT, &count);
+        return count;
     }
 };
