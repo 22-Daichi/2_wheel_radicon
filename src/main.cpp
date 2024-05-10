880d4f02e41b5b690b61ed542391993a3f10e2f8
Chikurin1014 <chikurin1014@gmail.com>
2024-05-10 14:57:17 +0900

    gamepadを実装

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
-#include <DabbleESP32.h>
-#include "driver/pcnt.h"
 
-#define CUSTOM_SETTINGS
-#define INCLUDE_GAMEPAD_MODULE
+#include "GamePad.hpp"
+
 #define motorAp 25
 #define motorBp 13
 #define pwmch_1 0
@@ -17,113 +15,111 @@ const int STBY = 12;
 const int motorB1 = 32; // IN1
 const int motorB2 = 33; // IN2
 
-void setup()
-{
-  pinMode(motorA1, OUTPUT);
-  pinMode(motorA2, OUTPUT);
-  pinMode(motorAp, OUTPUT);
+void setup() {
+    pinMode(motorA1, OUTPUT);
+    pinMode(motorA2, OUTPUT);
+    pinMode(motorAp, OUTPUT);
 
-  pinMode(STBY, OUTPUT);
+    pinMode(STBY, OUTPUT);
 
-  pinMode(motorB1, OUTPUT);
-  pinMode(motorB2, OUTPUT);
-  pinMode(motorBp, OUTPUT);
+    pinMode(motorB1, OUTPUT);
+    pinMode(motorB2, OUTPUT);
+    pinMode(motorBp, OUTPUT);
 
-  ledcSetup(pwmch_1, 12800, 8);
-  ledcSetup(pwmch_2, 12800, 8);
+    ledcSetup(pwmch_1, 12800, 8);
+    ledcSetup(pwmch_2, 12800, 8);
 
-  ledcAttachPin(motorAp, pwmch_1);
-  ledcAttachPin(motorBp, pwmch_2);
+    ledcAttachPin(motorAp, pwmch_1);
+    ledcAttachPin(motorBp, pwmch_2);
 
-  digitalWrite(motorA1, LOW);
-  digitalWrite(motorA2, LOW);
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, LOW);
 
-  digitalWrite(motorB1, LOW);
-  digitalWrite(motorB2, LOW);
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, LOW);
 
-  digitalWrite(STBY, HIGH);
+    digitalWrite(STBY, HIGH);
 
-  Dabble.begin("Bluth");
-  Serial.begin(9600);
-  Serial.println("Motor Standby");
+    Dabble.begin("Bluth");
+    Serial.begin(9600);
+    Serial.println("Motor Standby");
 }
 
-void loop()
-{
-  Dabble.processInput(); 
-  if (GamePad.isUpPressed())
-  {
-    ledcWrite(pwmch_1, 200);
-    digitalWrite(motorA1, HIGH);
-    digitalWrite(motorA2, LOW);
-    ledcWrite(pwmch_2, 200);
-    digitalWrite(motorB1, HIGH);
-    digitalWrite(motorB2, LOW);
-  }
+void loop() {
+    using game_pad::Button;
 
-  if (GamePad.isDownPressed())
-  {
-    // Serial.println("motorA back");
-    ledcWrite(pwmch_1, 200);
-    digitalWrite(motorA1, LOW);
-    digitalWrite(motorA2, HIGH);
-    // Serial.println("motorB back");
-    ledcWrite(pwmch_2, 200);
-    digitalWrite(motorB1, LOW);
-    digitalWrite(motorB2, HIGH);
-  }
-  if (GamePad.isCrossPressed())
-  {
-    // Serial.println("motorA stop");
-    digitalWrite(motorA1, LOW);
-    digitalWrite(motorA2, LOW);
-    ledcWrite(pwmch_1, 0);
-    // Serial.println("motorB stop");
-    digitalWrite(motorB1, LOW);
-    digitalWrite(motorB2, LOW);
-    ledcWrite(pwmch_2, 0);
-  }
-  if (GamePad.isCirclePressed())
-  {
-    // Serial.println("motorA back");
-    ledcWrite(pwmch_1, 200);
-    digitalWrite(motorA1, LOW);
-    digitalWrite(motorA2, HIGH);
-    // Serial.println("motorB forward");
-    ledcWrite(pwmch_2, 200);
-    digitalWrite(motorB1, HIGH);
-    digitalWrite(motorB2, LOW);
-  }
-
-  if (GamePad.isSquarePressed()){ 
-    // Serial.println("motorA forward");
-    ledcWrite(pwmch_1, 200);
-    digitalWrite(motorA1, HIGH);
-    digitalWrite(motorA2, LOW);
-    // rial.println("motorB back");
-    ledcWrite(pwmch_2, 200);
-    digitalWrite(motorB1, LOW);
-    digitalWrite(motorB2, HIGH);
-  }
-  if (GamePad.isRightPressed()){
-    // Serial.println("motorA slow");
-    ledcWrite(pwmch_1, 200);
-    digitalWrite(motorA1, HIGH);
-    digitalWrite(motorA2, LOW);
-    // Serial.println("motorB forward");
-    ledcWrite(pwmch_2, 256);
-    digitalWrite(motorB1, HIGH);
-    digitalWrite(motorB2, LOW);
-  }
-  if (GamePad.isLeftPressed())
-  {
-    // Serial.println("motorA forward");
-    ledcWrite(pwmch_1, 256);
-    digitalWrite(motorA1, HIGH);
-    digitalWrite(motorA2, LOW);
-    // Serial.println("motorB slow");
-    ledcWrite(pwmch_2, 200);
-    digitalWrite(motorB1, HIGH);
-    digitalWrite(motorB2, LOW);
-  }
+    Dabble.processInput();
+    Button button = game_pad::pressedButton();
+    switch (button) {
+        case Button::Up:
+            ledcWrite(pwmch_1, 200);
+            digitalWrite(motorA1, HIGH);
+            digitalWrite(motorA2, LOW);
+            ledcWrite(pwmch_2, 200);
+            digitalWrite(motorB1, HIGH);
+            digitalWrite(motorB2, LOW);
+            break;
+        case Button::Down:
+            // Serial.println("motorA back");
+            ledcWrite(pwmch_1, 200);
+            digitalWrite(motorA1, LOW);
+            digitalWrite(motorA2, HIGH);
+            // Serial.println("motorB back");
+            ledcWrite(pwmch_2, 200);
+            digitalWrite(motorB1, LOW);
+            digitalWrite(motorB2, HIGH);
+            break;
+        case Button::Left:
+            // Serial.println("motorA forward");
+            ledcWrite(pwmch_1, 256);
+            digitalWrite(motorA1, HIGH);
+            digitalWrite(motorA2, LOW);
+            // Serial.println("motorB slow");
+            ledcWrite(pwmch_2, 200);
+            digitalWrite(motorB1, HIGH);
+            digitalWrite(motorB2, LOW);
+            break;
+        case Button::Right:
+            // Serial.println("motorA slow");
+            ledcWrite(pwmch_1, 200);
+            digitalWrite(motorA1, HIGH);
+            digitalWrite(motorA2, LOW);
+            // Serial.println("motorB forward");
+            ledcWrite(pwmch_2, 256);
+            digitalWrite(motorB1, HIGH);
+            digitalWrite(motorB2, LOW);
+            break;
+        case Button::Circle:
+            // Serial.println("motorA back");
+            ledcWrite(pwmch_1, 200);
+            digitalWrite(motorA1, LOW);
+            digitalWrite(motorA2, HIGH);
+            // Serial.println("motorB forward");
+            ledcWrite(pwmch_2, 200);
+            digitalWrite(motorB1, HIGH);
+            digitalWrite(motorB2, LOW);
+            break;
+        case Button::Cross:
+            // Serial.println("motorA stop");
+            digitalWrite(motorA1, LOW);
+            digitalWrite(motorA2, LOW);
+            ledcWrite(pwmch_1, 0);
+            // Serial.println("motorB stop");
+            digitalWrite(motorB1, LOW);
+            digitalWrite(motorB2, LOW);
+            ledcWrite(pwmch_2, 0);
+            break;
+        case Button::Square:
+            // Serial.println("motorA forward");
+            ledcWrite(pwmch_1, 200);
+            digitalWrite(motorA1, HIGH);
+            digitalWrite(motorA2, LOW);
+            // rial.println("motorB back");
+            ledcWrite(pwmch_2, 200);
+            digitalWrite(motorB1, LOW);
+            digitalWrite(motorB2, HIGH);
+            break;
+        default:
+            break;
+    }
 }
